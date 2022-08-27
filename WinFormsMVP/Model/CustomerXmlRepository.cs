3044268c7454062f57ab769bdbd444fa01ef99f2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-27 18:08:42 +0300

    Add project files.

---
+++ WinFormsMVP/Model/CustomerXmlRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WinFormsMVP.Model
+{
+    internal class CustomerXmlRepository : ICustomerRepository
+    {
+        private readonly string _xmlFilePath;
+        private readonly XmlSerializer _serializer = new XmlSerializer(typeof(List<Customer>));
+        private readonly Lazy<List<Customer>> _customers;
+
+        public CustomerXmlRepository(string fullPath)
+        {
+            _xmlFilePath = fullPath + @"\customers.xml";
+
+            if (!File.Exists(_xmlFilePath))
+                CreateCustomerXmlStub();
+
+            _customers = new Lazy<List<Customer>>(() =>
+            {
+                using (var reader = new StreamReader(_xmlFilePath))
+                {
+                    return (List<Customer>)_serializer.Deserialize(reader);
+                }
+            });
+        }
+
+        private void CreateCustomerXmlStub()
+        {
+            var stubCustomerList = new List<Customer> {
+                new Customer {Name = "Berkan", Address = "Istanbul", Phone = "0531 556 92 35"},
+                new Customer {Name = "Ahmet", Address = "Ankara", Phone = "0531 556 92 36"},
+                new Customer {Name = "Mehmet", Address = "Izmir", Phone = "0531 556 92 37"}
+            };
+            SaveCustomerList(stubCustomerList);
+        }
+
+        private void SaveCustomerList(List<Customer> customers)
+        {
+            using (var writer = new StreamWriter(_xmlFilePath, false))
+            {
+                _serializer.Serialize(writer, customers);
+            }
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _customers.Value;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            return _customers.Value[id];
+        }
+
+        public void SaveCustomer(int id, Customer customer)
+        {
+            _customers.Value[id] = customer;
+            SaveCustomerList(_customers.Value);
+        }
+    }
+}
